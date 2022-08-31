970e6acbe99498092498e7d111ba2cb150637aa9
GabriellDev0 <gabrielldev0@gmail.com>
2022-08-31 19:50:29 -0300

    Adicionada a funcionalidade de Active Link Class

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,26 @@
+import { ReactElement, cloneElement } from 'react'
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+
+
+//Vai estender também o ...rest das propriedades do Link.
+interface ActiveLinkProps extends LinkProps{
+  children: ReactElement
+  activeClassName: string;
+}
+
+const index = ({children, activeClassName, ...rest}:ActiveLinkProps) => {
+  const { asPath } = useRouter();  // /algumaCoisa
+
+  const className = asPath === rest.href ? activeClassName : ''
+  // Se a rota/pagina que estamos acessando for igual ao link que ele clicou, então ativamos o Active Class.
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className
+      })}
+    </Link>
+  )
+}
+
+export default index
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,27 +4,37 @@ import styles from './header.styles.module.scss';
 import Image from 'next/image';
 import logo from '../../../public/images/logo.svg';
 
-import Link from 'next/link';
+import ActiveLink from '../ActiveLink/index';
 
 const header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <a>
-          <Image src={logo} alt="Dev Resiliente Logo" />
-        </a>
+        <ActiveLink href="/" activeClassName={styles.active}>
+          <a>
+            <Image src={logo} alt="Dev Resiliente Logo" />
+          </a>
+        </ActiveLink>
         <nav>
-          <Link href="/">
+          <ActiveLink href="/" activeClassName={styles.active}>
             <a>Home</a>
-          </Link>
-          <Link href="/">
+          </ActiveLink>
+
+          <ActiveLink href="/posts" activeClassName={styles.active}>
             <a>Conteúdos</a>
-          </Link>
-          <Link href="/">
+          </ActiveLink>
+
+          <ActiveLink href="/sobre" activeClassName={styles.active}>
             <a>Quem somos?</a>
-          </Link>
+          </ActiveLink>
         </nav>
-        <a className={styles.readyButton} type="button" href="www.instagram.com/gabriel.dev_">COMEÇAR</a>
+        <a
+          className={styles.readyButton}
+          type="button"
+          href="www.instagram.com/gabriel.dev_"
+        >
+          COMEÇAR
+        </a>
       </div>
     </header>
   );
